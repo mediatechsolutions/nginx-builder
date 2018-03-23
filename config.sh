0f2821353f383dcb9b8d5b8e083ac6451331f51c
Miguel Angel Garcia <miguel.garcia@mediatechsolutions.es>
2018-03-23 09:36:44 +0100

    add more variables

--- config.sh
+++ config.sh
@@ -8,12 +8,17 @@ declare SCRIPT_PATH=$PWD/
 # ROOT:: path where all the compiling is done (this is NOT path to installed nginx)
 declare -A ROOT=$(eval echo ~${SUDO_USER})"/nginx-build/" #/opt
 # NGINX_PATH:: path where nginx will be located. Default is /usr/local/nginx -- don't forget trailing /
-declare NGINX_PATH="/usr/local/nginx/"
+declare NGINX_PATH=${NGINX_PATH:-/usr/local/nginx/}
 declare NGINX_USE_PATH="/etc/nginx/"
 declare NGINX_VERSION_NO=""
 declare NGINX_SERVER_URL="example.com" #
 declare NGINX_PROJECT_NAME="example" #
 declare NGINX_SERVER_PORT="80" #
+declare NGINX_SBIN_PATH=${NGINX_SBIN_PATH:-sbin/nginx}
+declare NGINX_CONFIG_PATH=${NGINX_CONFIG_PATH:-nginx.conf}
+declare NGINX_ERRORLOG_PATH=${NGINX_ERRORLOG_PATH:-${NGINX_PATH}/logs/error.log}
+declare NGINX_ACCESSLOG_PATH=${NGINX_ACCESSLOG_PATH:-${NGINX_PATH}/logs/access.log}
+declare NGINX_PID_PATH=${NGINX_PID_PATH:-${NGINX_PATH}/logs/nginx.pid}
 
 
 declare -A CACHE="${ROOT}cache/"
@@ -37,7 +42,11 @@ declare DISTRO_VERSION=$(lsb_release -sr)
 
 
 # Default: build params
-DEFAULT_CONFIGURE_PARAMS+="--prefix=${NGINX_PATH} --sbin-path=sbin/nginx --conf-path=nginx.conf --pid-path=logs/nginx.pid --user=www-data "
+DEFAULT_CONFIGURE_PARAMS+="--prefix=${NGINX_PATH} "
+DEFAULT_CONFIGURE_PARAMS+="--sbin-path=${NGINX_SBIN_PATH} --pid-path=${NGINX_PID_PATH} "
+DEFAULT_CONFIGURE_PARAMS+="--conf-path=${NGINX_CONFIG_PATH} "
+DEFAULT_CONFIGURE_PARAMS+="--error-log-path=${NGINX_ERRORLOG_PATH} -http-log-path=${NGINX_ACCESSLOG_PATH} "
+DEFAULT_CONFIGURE_PARAMS+="--user=www-data "
 DEFAULT_CONFIGURE_PARAMS+="--with-pcre=../pcre --with-zlib=../zlib "
 DEFAULT_CONFIGURE_PARAMS+="--with-http_realip_module  --with-http_gzip_static_module --with-stream "
 DEFAULT_CONFIGURE_PARAMS+="--with-stream_ssl_preread_module --with-compat "
